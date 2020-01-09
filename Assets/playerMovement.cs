4629295bf47e3f231d93ed282f658620461eee92
LEGODesigner26 <58293495+LEGODesigner26@users.noreply.github.com>
2020-01-09 15:19:29 +0100

    First commit with some shit
    Eerste lading met tiles en de player met movement script

---
+++ Assets/playerMovement.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+
+    public float moveSpeed;
+    public float sprintProduct;
+    public bool sprintState;
+
+    public Rigidbody2D rb;
+
+    public Animator animator;
+
+    Vector2 movement;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //input 
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+
+        if (Input.GetKey(KeyCode.LeftShift) == true && movement.sqrMagnitude != 0)
+        {
+            sprintState = true;
+        }
+        else sprintState = false;
+    }
+
+    void FixedUpdate()
+    {
+        //movement 
+        if(sprintState == false)
+        {
+            rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        }
+        else if (sprintState == true)
+        {
+            rb.MovePosition(rb.position + movement * moveSpeed * sprintProduct * Time.fixedDeltaTime);
+        }
+    }
+}
