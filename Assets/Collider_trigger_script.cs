4fbcc711109fa2813c3490787abcdbf9f2acf1db
LEGODesigner26 <58293495+LEGODesigner26@users.noreply.github.com>
2020-01-20 19:35:48 +0100

    Scripts, collisions fixed

---
+++ Assets/Collider_trigger_script.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collider_trigger_script : MonoBehaviour
+{
+    public int sortingOrder = 0;
+    private SpriteRenderer player;
+
+    private void OnTriggerEnter2D(UnityEngine.Collider2D collision)
+    {
+        player = collision.GetComponent<SpriteRenderer>();
+
+        player.sortingOrder = 2;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        player = collision.GetComponent<SpriteRenderer>();
+
+        player.sortingOrder = 0;
+    }
+}
---
+++ Assets/Layerposition.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Layerposition : MonoBehaviour
+{
+    public int sortingOrder = 0;
+    private SpriteRenderer sprite;
+    
+    void Start()
+    
+    {
+    
+        sprite = GetComponent<SpriteRenderer>();
+
+        if (sprite)
+        {
+
+            sprite.sortingOrder = sortingOrder;
+        }
+    }
+}
