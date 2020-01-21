1f9ce35d8652514c2303464c6cefe33181b80490
LEGODesigner26 <58293495+LEGODesigner26@users.noreply.github.com>
2020-01-21 18:19:47 +0100

    Rewrote the script for entering and leaving another floor.

---
+++ Assets/Enter_New_Floor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enter_New_Floor : MonoBehaviour
+{
+    public int sortingOrder = 1;
+    public SpriteRenderer spr;
+    public Collider2D plrCol;
+    public GameObject uprFlr;
+    public Collider2D grdFlr;
+    public Collider2D grdFlrEXTRA;
+
+
+    private void OnTriggerEnter2D(UnityEngine.Collider2D collision)
+    {
+        if (collision == plrCol)
+        {
+            spr = collision.GetComponent<SpriteRenderer>();
+            uprFlr.SetActive(true);
+            grdFlr.enabled = false;
+            grdFlrEXTRA.enabled = false;
+            spr.sortingOrder = 2;
+        }
+    }
+}
---
+++ Assets/Leave_New_Floor.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Leave_New_Floor : MonoBehaviour
+{
+    public int sortingOrder = 2;
+    public SpriteRenderer spr;
+    public Collider2D plrCol;
+    public GameObject uprFlr;
+    public Collider2D grdFlr;
+    public Collider2D grdFlrEXTRA;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision == plrCol)
+        {
+            spr = collision.GetComponent<SpriteRenderer>();
+            uprFlr.SetActive(false);
+            grdFlr.enabled = true;
+            grdFlrEXTRA.enabled = true;
+            spr.sortingOrder = 1;
+        }
+    }
+}
