47bd0aed5735dad507c79b5200f5bd1c93255218
Fliedrice <Jingmingchia@hotmail.com>
2020-01-22 07:25:43 +0100

    Finished mini game basic mechanics

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public Quesition[] questions;
+    private static List<Quesition> unansweredQuestions;
+    private Quesition currentQuestion;
+
+    [SerializeField]
+    private Text factText;
+
+    [SerializeField]
+    private float timeBetweenQuetions = 1f;
+
+    private void Start()
+    {
+        if( unansweredQuestions == null)
+        {
+            unansweredQuestions = questions.ToList<Quesition>(); 
+        }
+        if (unansweredQuestions.Count == 0)
+        {
+            SceneManager.LoadScene("Atlas Scene");
+        }
+        getQuestion();
+
+    }
+
+   
+    
+    void getQuestion()
+    {
+        currentQuestion = unansweredQuestions[0];
+        factText.text = currentQuestion.fact;
+        Debug.Log("Got Question");
+
+    }
+
+    IEnumerator TransitionToNextQuestion()
+    {
+        unansweredQuestions.Remove(currentQuestion);
+        yield return new WaitForSeconds(timeBetweenQuetions);
+        string scene = SceneManager.GetActiveScene().name;
+        SceneManager.LoadScene(scene);
+
+    }
+
+    public void userSelectedTrue()
+    {
+        if(currentQuestion.isTrue)
+        {
+            Debug.Log("True Is True");
+        }
+        else
+        {
+            Debug.Log("Wrong cuz it need to eb true");
+        }
+        StartCoroutine(TransitionToNextQuestion());
+    }
+
+    public void userSelectedFalse()
+    {
+        if (!currentQuestion.isTrue)
+        {
+            Debug.Log("Correct");
+        }
+        else
+        {
+            Debug.Log("Wrong");
+        }
+    }
+
+}
---
+++ Assets/Scripts/Question.cs
@@ -0,0 +1,11 @@
+﻿
+[System.Serializable]
+public struct Quesition
+{
+    public string fact;
+    public bool isTrue;
+
+}
+
+
+
