1ebaf647df85fff71cf83d3a201509d1781cd40e
LEGODesigner26 <58293495+LEGODesigner26@users.noreply.github.com>
2020-01-23 01:28:52 +0100

    Shade when ont the first floor, and more decorations

--- Assets/Scripts/Enter_New_Floor.cs
+++ Assets/Scripts/Enter_New_Floor.cs
@@ -12,13 +12,29 @@ public class Enter_New_Floor : MonoBehaviour
     public GameObject upFlrSPRITES;
     public Collider2D grdFlr;
     public Collider2D grdFlrEXTRA;
-
-
+    private GameObject[] List1;
+    private GameObject[] List2;
+    private void Start()
+    {
+       List1 = GameObject.FindGameObjectsWithTag("Furniture Ground");
+        List2 = GameObject.FindGameObjectsWithTag("Furniture 1st");
+    }
     private void OnTriggerEnter2D(UnityEngine.Collider2D collision)
     {
         if (collision == plrCol)
         {
             spr = collision.GetComponent<SpriteRenderer>();
+
+            foreach (GameObject Furniture in List1)
+            {
+                Furniture.GetComponent<PolygonCollider2D>().enabled = false;
+            }
+
+            foreach (GameObject Furniture in List2)
+            {
+                Furniture.GetComponent<PolygonCollider2D>().enabled = true;
+            }
+            
             uprFlr.SetActive(true);
             upFlrDOORS.SetActive(true);
             grdFlr.enabled = false;
--- Assets/Scripts/Leave_New_Floor.cs
+++ Assets/Scripts/Leave_New_Floor.cs
@@ -12,12 +12,30 @@ public class Leave_New_Floor : MonoBehaviour
     public GameObject upFlrSPRITES;
     public Collider2D grdFlr;
     public Collider2D grdFlrEXTRA;
+    public GameObject[] List1;
+    private GameObject[] List2;
+    private void Start()
+    {
+        List1 = GameObject.FindGameObjectsWithTag("Furniture Ground");
+         List2 = GameObject.FindGameObjectsWithTag("Furniture 1st");
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision == plrCol)
         {
             spr = collision.GetComponent<SpriteRenderer>();
+
+            foreach (GameObject Furniture in List1)
+            {
+                Furniture.GetComponent<PolygonCollider2D>().enabled = true;
+            }
+
+            foreach (GameObject Furniture in List2)
+            {
+                Furniture.GetComponent<PolygonCollider2D>().enabled = false;
+            }
+
             uprFlr.SetActive(false);
             upFlrSPRITES.SetActive(false);
             upFlrDOORS.SetActive(false);
